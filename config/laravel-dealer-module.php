b626cfbca683630be7d885c30b7a521aab4fb8df
erenMustafaOzdal <eren.060737@gmail.com>
2017-01-12 03:39:25 +0000

    config cache için ayarlar düzenlendi

--- config/laravel-dealer-module.php
+++ config/laravel-dealer-module.php
@@ -51,16 +51,27 @@ return [
     */
     'routes' => [
         'admin' => [
-            'dealer_category'           => true,        // Is the route to be used categories admin
-            'dealer'                    => true,        // Is the route to be used dealers admin
-            'nested_sub_categories'     => true,        // Did subcategory nested categories admin route will be used
-            'sub_category_dealers'      => true,        // Did subcategory dealer admin route will be used
+            'dealer_category'               => true,        // admin dealer category resource route
+            'dealer'                        => true,        // admin dealer resource route
+            'dealer_publish'                => true,        // admin dealer publish get route
+            'dealer_notPublish'             => true,        // admin dealer not publish get route
+            'category_categories'           => true,        // admin category nested categories resource route
+            'category_dealers'              => true,        // admin category dealers resource route
+            'category_dealers_publish'      => true,        // admin category dealers publish get route
+            'category_dealers_notPublish'   => true         // admin category dealers not publish get route
         ],
         'api' => [
-            'dealer_category'           => true,        // Is the route to be used categories api
-            'dealer'                    => true,        // Is the route to be used dealers api
-            'nested_sub_categories'     => true,        // Did subcategory nested categories api route will be used
-            'sub_category_dealers'      => true,        // Did subcategory dealer api route will be used
+            'dealer_category'               => true,        // api dealer category resource route
+            'dealer_category_models'        => true,        // api dealer category model post route
+            'dealer_category_move'          => true,        // api dealer category move post route
+            'dealer'                        => true,        // api dealer resource route
+            'dealer_group'                  => true,        // api dealer group post route
+            'dealer_detail'                 => true,        // api dealer detail get route
+            'dealer_fastEdit'               => true,        // api dealer fast edit post route
+            'dealer_publish'                => true,        // api dealer publish post route
+            'dealer_notPublish'             => true,        // api dealer not publish post route
+            'category_categories_index'     => true,        // api category nested categories index get route
+            'category_dealers_index'        => true,        // api category dealers index get route
         ]
     ],
 
--- src/LaravelDealerModuleServiceProvider.php
+++ src/LaravelDealerModuleServiceProvider.php
@@ -39,65 +39,10 @@ class LaravelDealerModuleServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/laravel-dealer-module.php', 'laravel-dealer-module'
         );
-        // merge default configs with publish configs
-        $this->mergeDefaultConfig();
 
         $router = $this->app['router'];
         // model binding
         $router->model(config('laravel-dealer-module.url.dealer'),  'App\Dealer');
         $router->model(config('laravel-dealer-module.url.dealer_category'),  'App\DealerCategory');
-    }
-
-    /**
-     * merge default configs with publish configs
-     */
-    protected function mergeDefaultConfig()
-    {
-        $config = $this->app['config']->get('laravel-dealer-module', []);
-        $default = require __DIR__.'/../config/default.php';
-
-        // admin dealer category routes
-        $route = $config['routes']['admin']['dealer_category'];
-        $default['routes']['admin']['dealer_category'] = $route;
-        // admin dealer routes
-        $route = $config['routes']['admin']['dealer'];
-        $default['routes']['admin']['dealer'] = $route;
-        $default['routes']['admin']['dealer_publish'] = $route;
-        $default['routes']['admin']['dealer_notPublish'] = $route;
-        // admin sub dealer categories nested categories
-        $route = $config['routes']['admin']['nested_sub_categories'];
-        $default['routes']['admin']['category_categories'] = $route;
-        // admin sub dealer categories dealers
-        $route = $config['routes']['admin']['sub_category_dealers'];
-        $default['routes']['admin']['category_dealers'] = $route;
-        $default['routes']['admin']['category_dealers_publish'] = $route;
-        $default['routes']['admin']['category_dealers_notPublish'] = $route;
-
-        // api dealer category routes
-        $apiCat = $config['routes']['api']['dealer_category'];
-        $default['routes']['api']['dealer_category'] = $apiCat;
-        // api sub dealer categories nested categories
-        $apiSubCat = $config['routes']['api']['nested_sub_categories'];
-        $default['routes']['api']['category_categories_index'] = $apiSubCat;
-
-        $default['routes']['api']['dealer_category_models'] = $apiCat || $apiSubCat;
-        $default['routes']['api']['dealer_category_move'] = $apiCat || $apiSubCat;
-
-        // api dealer routes
-        $model = $config['routes']['api']['dealer'];
-        $default['routes']['api']['dealer'] = $model;
-        // api sub dealer categories dealers
-        $subModel = $config['routes']['api']['sub_category_dealers'];
-        $default['routes']['api']['category_dealers_index'] = $subModel;
-
-        $default['routes']['api']['dealer_group'] = $model || $subModel;
-        $default['routes']['api']['dealer_detail'] = $model || $subModel;
-        $default['routes']['api']['dealer_fastEdit'] = $model || $subModel;
-        $default['routes']['api']['dealer_publish'] = $model || $subModel;
-        $default['routes']['api']['dealer_notPublish'] = $model || $subModel;
-
-        $config['routes'] = $default['routes'];
-
-        $this->app['config']->set('laravel-dealer-module', $config);
     }
 }
